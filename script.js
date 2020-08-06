49b62e77aa90ef7b9b3d82c22fb3aebfb7bc5f04
leenades <57545008+leenades@users.noreply.github.com>
2020-08-06 08:00:46 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-$('#searchBox').hide();
-$('#searchIcon').on('click', function(){
-    $('#searchBox').toggle(800);
-});
+// $('#searchBox').hide();
+// $('#searchIcon').on('click', function(){
+    // $('#searchBox').toggle(800);
+// });
 
 
-window.onload = function counter(){
-    var start = 0;
+// window.onload = function counter(){
+//     var start = 0;
 
 
-}
+// }
