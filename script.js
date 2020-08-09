b7f6611640d35940b130cb5ee0399877f1777117
leenades <leenasalman.11@gmail.com>
2020-08-09 14:10:27 +0300

    commit

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 var element = $('#searchBox');
-$('#searchIcon').click(function(){
+var navbarButton = $('.container-nav-links');
+
+
+$('#searchIcon').click(function(event){
+    event.preventDefault();
     element.toggleClass('search-box-field-new');
+});
+$(window).resize(function(){
+    navbarButton.removeClass('mobileTransition');
+});
+
+$('#btn-menu').click(function(){
+    if(!navbarButton.hasClass('mobileTransition')){
+        navbarButton.addClass('mobileTransition');
+    }
+    navbarButton.toggleClass('transitionNavBar');
 });
