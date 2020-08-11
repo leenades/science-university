8e2aef7a4e5340b2ceece6a79c765bec6849ee9f
leenades <leenasalman.11@gmail.com>
2020-08-11 11:58:16 +0300

    initial commit

--- JS/counter.js
+++ JS/counter.js
@@ -1,46 +1,47 @@
 
-$(window).scroll(function(){
-    var wh = $(window).scrollTop();
+$(window).on('load scroll', function(){
+    var wh = $(window).scrollTop() + $(window).height();
     // >= $('.news-btn').offset().top && wh < $('.ticker-header-title').offset().top
-    if (wh <= $('.news-content').offset().top){
-            var countersUp = document.querySelectorAll('.counterInc');
-            var countersDown = document.querySelectorAll('.counterDecr');
+    if (wh >= $('.section-ticker').offset().top && !$('.section-ticker').hasClass('reached')){
+        $('.section-ticker').addClass('reached');
+        var countersDown = document.querySelectorAll('.counterDecr');
+        var countersUp = document.querySelectorAll('.counterInc');
+        
+        countersUp.forEach(counter => {
+            counter.innerText = 0;
             
-            countersUp.forEach(counter => {
-                counter.innerText = 0;
+            var updateCounter = () => {
+                var target = +counter.getAttribute('data-target');
                 
-                var updateCounter = () => {
-                    var target = +counter.getAttribute('data-target');
-                    
-                    var c = +counter.innerText;
-                    
-                    var inc = target / 1000;
-                    
-                    if(c < target){
-                        counter.innerText = Math.ceil(c + inc);
-                        setTimeout(updateCounter, 1);
-                    } else {
-                        counter.innerText = target;
-                    }
+                var c = +counter.innerText;
+                
+                var inc = target / 100;
+                
+                if(c < target){
+                    counter.innerText = Math.ceil(c + inc);
+                    setTimeout(updateCounter, 8);
+                } else {
+                    counter.innerText = target;
                 }
-                updateCounter();
-            });
+            }
+            updateCounter();
+        });
+        
+        countersDown.forEach(counter =>{
+            counter.innerText = 100;
             
-            countersDown.forEach(counter =>{
-                counter.innerText = 100;
+            var updateCounter = () =>{
+                var target = +counter.getAttribute('data-target'); // 1
                 
-                var updateCounter = () =>{
-                    var target = +counter.getAttribute('data-target'); // 1
-                    
-                    var c = +counter.innerText; // 100
-                    if(c > target){
-                        counter.innerText = c - 1;
-                        setTimeout(updateCounter, 2);
-                    } else {
-                        counter.innerText = target;
-                    };
-                }
-                updateCounter();
-            });
-        }
+                var c = +counter.innerText; // 100
+                if(c > target){
+                    counter.innerText = c - 1;
+                    setTimeout(updateCounter, 8);
+                } else {
+                    counter.innerText = target;
+                };
+            }
+            updateCounter();
+        });
+    }
 });
