0782853bcb55d82fc9195dd1a4198cfcc56c0313
leenades <leenasalman.11@gmail.com>
2020-09-03 17:04:35 +0300

    commit changes html to php

---
+++ Pages/about.php
@@ -0,0 +1,128 @@
+<?php 
+include_once '../includes/navbar.php';
+?>
+
+    <!-- ABOUT SECTION HEADER START -->
+        <section class="about-header-section">
+            <div class="about-header-img">
+                <img
+                src="../Assets/Images/headersun1.jpg"
+                alt="Science University Campus"
+                />
+            </div>
+        </section>
+        <!-- ABOUT SECTION HEADER END -->
+
+        <!-- About Section START -->
+        <section class="about-section">
+            <div class="container">
+                <div class="row about-title-container"> <!-- TITLE -->
+                    <div class="col blurb-news">
+                        <h2 class="title-style">About Science University</h2>
+                    </div>
+                </div>
+                <div class="row about-info-image">
+                    <div class="col about-info">
+                        <p>
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                            Consequuntur, nesciunt rem vel ipsam neque voluptatum harum 
+                            saepe facere rerum omnis? Maiores, perspiciatis assumenda 
+                            impedit aliquid explicabo aut aperiam quae. Dolore.
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                            Consequuntur, nesciunt rem vel ipsam neque voluptatum harum 
+                            saepe facere rerum omnis? Maiores, perspiciatis assumenda 
+                            impedit aliquid explicabo aut aperiam quae. Dolore. 
+                        </p>
+                    </div>
+                    <div class="col about-img">
+                        <img 
+                        class="w-100" 
+                        src="../Assets/Images/sliderimg5.jpg" 
+                        alt="Science University Campus" />
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- About Section END -->
+
+        <!-- Why Science Uni START -->
+        <section class="why-science-uni-section">
+            <div class="container">
+                <div class="row why-science-uni-title-container about-title-container">
+                    <div class="col blurb-news">
+                        <h2 class="title-style">Why Science University?</h2>
+                    </div>
+                </div>
+            <div class="row why-science-uni-info">
+                <div class="col">
+                    <div class="icon-container">
+                        <div class="row">
+                            <div class="col-3">
+                                <span>
+                                    <i class="fa fa-circle-o" aria-hidden="true"></i>
+                                </span>
+                            </div>
+                            <div class="col">
+                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                                    Cum sint, magnam nobis fugit voluptate accusamus temporibus.
+                                </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="icon-container">
+                            <div class="row">
+                                <div class="col-3">
+                                    <span>
+                                        <i class="fa fa-pie-chart" aria-hidden="true"></i>
+                                    </span>
+                                </div>
+                            <div class="col">
+                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                                    Cum sint, magnam nobis fugit voluptate accusamus temporibus.
+                                </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="w-100"></div>
+                    <div class="col">
+                        <div class="icon-container">
+                        <div class="row">
+                            <div class="col-3">
+                                <span>
+                                    <i class="fa fa-spinner" aria-hidden="true"></i>
+                                </span>
+                            </div>
+                            <div class="col">
+                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                                    Cum sint, magnam nobis fugit voluptate accusamus temporibus.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="icon-container">
+                        <div class="row">
+                            <div class="col-3">
+                                <span>
+                                    <i class="fa fa-registered" aria-hidden="true"></i>
+                                </span>
+                            </div>
+                            <div class="col">
+                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                                    Cum sint, magnam nobis fugit voluptate accusamus temporibus.
+                                </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Why Science Uni END -->
+<?php
+include_once '../includes/footer.php';
+?>
---
+++ includes/navbar.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Science University</title>
+        <meta charser="utf-8">
+        <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge" /> -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../bootstrap-4.5.0\dist\css\bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="../themes/css/GLOBAL.css">
+        <link rel="stylesheet" href="../themes/css/navbar.css">
+        <link rel="stylesheet" href="../themes/css/header.css">
+        <link rel="stylesheet" href="../themes/css/news-courses.css">
+        <link rel="stylesheet" href="../themes/css/ticker.css">
+        <link rel="stylesheet" href="../themes/css/events.css">
+        <link rel="stylesheet" href="../themes/css/admission.css">
+        <link rel="stylesheet" href="../themes/css/form.css">
+        <link rel="stylesheet" href="../themes/css/footer.css">
+        <link rel="stylesheet" href="../themes/css/animation.css">
+        <link rel="stylesheet" href="../themes/css/fonts.css">
+
+
+    </head>
+    <body>
+        <script
+        src="https://code.jquery.com/jquery-3.5.1.js"
+        integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
+        crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" 
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" 
+        crossorigin="anonymous"></script>
+        <script src="../bootstrap-4.5.0/dist/js/bootstrap.min.js"></script>
+        
+        <div class="search-box-transition">
+            <span class="close-search-box" id="close-search-box">
+                <i class="fa fa-slash first-child"></i>
+                <i class="fa fa-slash second-child"></i>
+            </span>
+            <div class="input-border-style">
+                <input type="search" class="search-box-field" id="searchBox" placeholder="Search...">
+            </div>
+        </div>
+        <!-- NAVBAR Section START -->
+        <nav class="navbar navbar-expand-xl navbar-styles">
+            <div class="navbar-brand-container">
+                <a class="navbar-brand" href="index.php"><img src="../Assets/Images/sciences-uni-logo.png" alt="science-university-logo"></a>
+            </div>
+            <button 
+            id="btn-menu"
+            class="navbar-toggler btn-menu-burger" 
+            type="button" 
+            data-toggle="collapse" 
+            data-target="#navBarCollapse" 
+            aria-controls="navBarCollapse" 
+            aria-expanded="false" 
+            aria-label="Toggle navigation">
+            <span class="dark-blue-text">
+                <i class="fa fa-align-right"></i>
+            </span>
+            </button>
+            <div class="search-social-container d-flex">
+                <div class="logo-socials-row">
+                    <div class="col social-media-icons-col">
+                        <div class="social-media-container">
+                            <a class="social-media-icon" href="#" aria-label="Linkedin Social Media Icon"><i class="fa fa-linkedin"></i></a>
+                            <a class="social-media-icon" href="#" aria-label="Youtube Social Media Icon"><i class="fa fa-youtube"></i></a>
+                            <a class="social-media-icon" href="#" aria-label="Instagram Social Media Icon"><i class="fa fa-instagram"></i></a>
+                            <a class="social-media-icon" href="#" aria-label="Twitter Social Media Icon"><i class="fa fa-twitter"></i></a>
+                            <a class="social-media-icon" href="#" aria-label="Facebook Social Media Icon"><i class="fa fa-facebook"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="search-box-container">
+                    <a class="social-media-icon-search" id="searchIcon">
+                    <i class="fa fa-search"></i></a>
+                </div>
+            </div>
+            <div class="navbar-container collapse navbar-collapse" id="navBarCollapse">
+                <div class="container-fluid container-nav-links">
+                    <div class="row navbar-links">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="../about.php" aria-label="About">About</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#" aria-label="Academics">Academics</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#" aria-label="Admission">Admissions</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#" aria-label="International">International</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#" aria-label="Events">Events</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#" aria-label="Contact Us">Contact Us</a>
+                            </li>
+                            <div class="logo-socials-row-mobile d-xl-none">
+                                <div class="social-media-icons-col">
+                                    <div class="social-media-container">
+                                        <a class="social-media-icon" href="#" aria-label="Linkedin Social Media Icon"><i class="fa fa-linkedin"></i></a>
+                                        <a class="social-media-icon" href="#" aria-label="Youtube Social Media Icon"><i class="fa fa-youtube"></i></a>
+                                        <a class="social-media-icon" href="#" aria-label="Instagram Social Media Icon"><i class="fa fa-instagram"></i></a>
+                                        <a class="social-media-icon" href="#" aria-label="Twitter Social Media Icon"><i class="fa fa-twitter"></i></a>
+                                        <a class="social-media-icon" href="#" aria-label="Facebook Social Media Icon"><i class="fa fa-facebook"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <!-- NAVBAR Section END -->
